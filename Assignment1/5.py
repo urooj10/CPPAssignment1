7e7b21b289181a7c39a118974bbeb7ee0d0a13e3
urooj10 <42898445+urooj10@users.noreply.github.com>
2019-11-02 21:42:30 -0700

    updated 5.py

--- Assignment1/5.py
+++ Assignment1/5.py
@@ -2,7 +2,7 @@ fn=input("Enter your first name: ")
 
 ln=input("Enter your last name: ")
 fullname=fn+" "+ln
-print("your full name is: "+fn+" "+ln)
+print("your full name is: ",fullname)
 revname=list(reversed(fullname))
 print("your reversed full name is :",end="")
 for i in revname:
